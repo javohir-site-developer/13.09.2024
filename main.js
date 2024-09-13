cce2763d47e6ab0f93a3ea0a12408fe2c9757701
javohir-site-developer <javohirjasurovich999@gmail.com>
2024-09-13 22:15:38 +0500

    javohirniki

---
+++ main.js
@@ -0,0 +1,166 @@
+//one
+let one = "324";
+
+one = Number(one);
+
+console.log(one);
+
+//one.second
+
+let second = "324e-1";
+
+second = Number(second);
+
+console.log(second);
+
+//second
+
+let third = true;
+
+third = Number(third);
+
+console.log(third);
+
+//second.one
+
+let four = false;
+
+four = Number(four);
+
+console.log(four);
+
+//third
+
+let five = " ";
+
+five = Number(five);
+
+console.log(five);
+
+//third.second
+
+let six = null;
+
+six = Number(six);
+
+console.log(six);
+
+//four
+
+let seven = "hello";
+
+seven = Number(seven);
+
+console.log(seven);
+
+//four.second
+
+let eight = undefined;
+
+eight = Number(eight);
+
+console.log(eight);
+
+//main
+
+let nine = NaN;
+
+nine = Number(nine);
+
+console.log(nine);
+
+//main
+
+let ten = parseInt("20.01");
+
+ten = Number(ten);
+
+console.log(ten);
+
+//main
+
+let eleven = parseFloat("20.01");
+
+eleven = Number(eleven);
+
+console.log(eleven);
+
+//main
+
+let dataOne = null;
+
+dataOne = String(dataOne);
+
+console.log(dataOne);
+
+//mine
+
+let dataTwo = undefined;
+
+dataTwo = String(dataTwo);
+
+console.log(dataTwo);
+
+//mine
+
+let dataFour = NaN;
+
+dataFour = String(dataFour);
+
+console.log(dataFour);
+
+//mine
+
+let dataFive = true;
+
+dataFive = String(dataFive);
+
+console.log(dataFive);
+
+//mine
+
+let dataSix = false;
+
+dataSix = String(dataSix);
+
+console.log(dataSix);
+
+//mine
+
+let boolen1 = null;
+
+boolen1 = Boolean(boolen1);
+
+console.log(boolen1);
+
+//mine
+
+let boolen2 = undefined;
+
+boolen2 = Boolean(boolen2);
+
+console.log(boolen2);
+
+//main
+
+let boolen3 = 0;
+
+boolen3 = Boolean(boolen3);
+
+console.log(boolen3);
+
+//main
+
+let boolen4 = " ";
+
+boolen4 = Boolean(boolen4);
+
+console.log(boolen4);
+
+//main
+
+let boolen5 = NaN;
+
+boolen5 = Boolean(boolen5);
+
+console.log(boolen5);
